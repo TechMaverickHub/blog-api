128d59d538f6c59c903b6b07bfd2d95ca921afb1
Abhiroop <abhiroop1998.dev@gmail.com>
2025-06-24 10:26:09 +0530

    Removed uuid as id in category model

--- app/category/models.py
+++ app/category/models.py
@@ -1,12 +1,10 @@
-import uuid
-
 from django.db import models
+
 
 # Create your models here.
 class Category(models.Model):
     """Blog post categories like 'Tech', 'Life', 'Travel'"""
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100, unique=True)
 
     # Additional field declarations
--- app/category/urls.py
+++ app/category/urls.py
@@ -5,7 +5,7 @@ from app.category.views import CategoryCreateAPIView, CategoryListFilterAPIView,
 urlpatterns = [
 
     path("", CategoryCreateAPIView.as_view(),name="post-create"),
-    path('<str:pk>', CategoryDetailAPI.as_view(), name='post-detail'),
+    path('<int:pk>', CategoryDetailAPI.as_view(), name='post-detail'),
     path('list-filter/', CategoryListFilterAPIView.as_view(), name='post-list-filter'),
 
 ]
